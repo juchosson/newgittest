57b8114bc9d4bec2597fe1d17edc8a1ec8660265
juchosson <juchosson@gmail.com>
2016-05-20 12:13:40 -0400

    Add files via upload

---
+++ contactthanks.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>Your message is sent</title>
+	
+</head>
+
+<body>
+		
+	<h1>Thank you for reaching out. Julien will come back at you as quickly as possible. We try to answer all emails within hours.</h1>
+
+</body>
+</html>
